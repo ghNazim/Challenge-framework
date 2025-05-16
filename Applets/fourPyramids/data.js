91c6d2504a3bf5f089abca5959ac96e7922f22d7
ghNazim <160389831+ghNazim@users.noreply.github.com>
2025-05-16 22:25:04 +0530

    working model ready

---
+++ Applets/fourPyramids/data.js
@@ -0,0 +1,70 @@
+const appTextContent = {
+  titleBar: {
+    heading: "Pyramids",
+    subheading: "Observe how a pyramid is formed fromits 2D base",
+  },
+  contextSteps: [
+    {
+      title: "Customize the radius",
+      text: "Adjust the radius of the cylinder by playing with the 'Radius' slider.",
+      highlights: [
+        {
+          word: "radius",
+          def: "radius",
+          instance: 1,
+          description:
+            "The radius of a cylinder is the distance from the center of the circular base to the edge of the circle.",
+        },
+      ],
+    },
+    {
+      title: "Customize the height",
+      text: "Adjust the height of the cylinder by playing with the 'Height' slider.",
+      highlights: [
+        {
+          word: "height",
+          def: "height",
+          instance: 1,
+          description:
+            "The height of a cylinder is the distance from the center of the circular base to the center of the circular top.",
+        },
+      ],
+    },
+    {
+      title: "Unravel the Cylinder",
+      text: "Use the 'Unwrap' slider to gradually unravel the cylinder. ",
+      highlights: [],
+    },
+    {
+      title: "Observe",
+      text: "The cylinder can be unwrapped into one rectangle and two circles.",
+      highlights: [
+        {
+          word: "rectangle",
+          def: "rectangle",
+          instance: 1,
+          description:
+            "A rectangle is a two-dimensional shape that has four straight sides and four right angles. Opposite sides are equal in length and parallel.",
+        },
+        {
+          word: "circles",
+          def: "circle",
+          instance: 1,
+          description:
+            "A circle is a round shape where every point on the edge is the same distance from the center.",
+        },
+      ],
+    },
+  ],
+  controls: {
+    sliderLabels: {
+      height: "Height",
+      form: "Form Pyramid",
+    },
+    buttons:["Square", "Rectangle", "Circle", "Triangle"],
+  },
+  fullscreenOverlay: {
+    title: "Full Screen Overlay",
+    text: "This overlay covers the entire applet with margins.",
+  },
+};
