e4683d65943245db4a0a827810f3be08a4d110ce
ghNazim <160389831+ghNazim@users.noreply.github.com>
2025-07-14 06:29:15 +0530

    multipli with objects on grid

---
+++ July/multiplicationOnGraphObject/applet/texts.js
@@ -0,0 +1,12 @@
+
+//LANGUAGE CHANGE
+
+const flag = "ENGLISH"; // <-- change this flag to"ENGLISH" or "INDONESIAN"
+
+
+//--------------------------------------//
+const questions = flag === "ENGLISH" ? questionsEnglish : questionsIndonesian;
+const leftInstructions = flag === "ENGLISH" ? leftInstructionsEnglish : leftInstructionsIndonesian;
+const overlayData = flag === "ENGLISH" ? overlayDataEnglish : overlayDataIndonesian;
+const buttonText = flag === "ENGLISH" ? buttonTextsEnglish : buttonTextsIndonesian;
+
